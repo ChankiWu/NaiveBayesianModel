80a9061f2612ab9f473b130c2f4221a860b3d226
ChankiWu <254152970@qq.com>
2017-03-30 20:37:11 +0800

    4.5.1 准备数据：从文本中构建词向量

---
+++ bayes.py
@@ -0,0 +1,26 @@
+
+def loadDataSet():
+    postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                  ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                  ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                  ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                  ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0, 1, 0, 1, 0, 1]  # 1 stands for insulting message,0 stands for normal statement
+    return postingList, classVec
+
+def createVocabList(dataSet):
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return list(vocabSet)
+
+def setOfWords2Vec(vocabList, inputSet):
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] = 1
+        else:
+            print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
