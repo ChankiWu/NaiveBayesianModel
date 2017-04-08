38064ddaee10a49bdc78692eb97f7a26dc970d6f
ChankiWu <254152970@qq.com>
2017-04-08 17:09:16 +0800

    4.5.3 测试算法：根据现实情况修改分类器 加入贝叶斯分类函数

--- bayes.py
+++ bayes.py
@@ -29,8 +29,12 @@ def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
     pAbusive = sum(trainCategory) / float(numTrainDocs)
-    p0Num = zeros(numWords); p1Num = zeros(numWords)
-    p0Denom = 0.0; p1Denom = 0.0
+
+    # p0Num = zeros(numWords); p1Num = zeros(numWords)
+    p0Num = ones(numWords); p1Num = ones(numWords)
+    #p0Denom = 0.0; p1Denom = 0.0
+    p0Denom = 2.0; p1Denom = 2.0 # maybe avoid 1/1
+
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -38,6 +42,34 @@ def trainNB0(trainMatrix, trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num / p1Denom         # change to log()
-    p0Vect = p0Num / p0Denom         # change to log()
+
+    # p1Vect = p1Num / p1Denom         # change to log()
+    # p0Vect = p0Num / p0Denom         # change to log()
+
+    p1Vect = log(p1Num / p1Denom)
+    p0Vect = log(p0Num / p0Denom)
+
     return p0Vect, p1Vect, pAbusive
+
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1 = sum(vec2Classify * p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify * p0Vec) + log(1.0 - pClass1)
+
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts,listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList,postinDoc))
+    p0V,p1V,pAb = trainNB0(array(trainMat),array(listClasses))
+    testEntry = ['love','my','dalmation']
+    thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
+    print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
+    testEntry = ['stupid','garbage']
+    thisDoc = array(setOfWords2Vec(myVocabList,testEntry))
+    print testEntry,'classified as: ',classifyNB(thisDoc,p0V,p1V,pAb)
